20ae80350bf0c5e9570b65b7d90844bdfca39246
matehat <mathieu@damours.org>
2014-06-03 17:25:50 -0400

    Added more sections to the prototype site

--- js/site.js
+++ js/site.js
@@ -1,1 +1,16 @@
-console.log(window.innerHeight);
+(function () {
+  var transitionTime = 0;
+  window.onscroll = function () {
+    console.log("S");
+    var $body = $('body');
+    if (Date.now() - transitionTime <= 200) return;
+    var distance = $(window).scrollTop() - $('#main').position().top;
+    if ($body.hasClass('anchored') && distance < -50) {
+      $body.removeClass('anchored');
+      transitionTime = Date.now();
+    } else if (!$body.hasClass('anchored') && distance >= 0) {
+      $body.addClass('anchored');
+      transitionTime = Date.now();
+    }
+  }
+})();
