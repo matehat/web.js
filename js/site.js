7efe0f30aeea9be456d61c15d4ee7ae60edb1cb9
matehat <mathieu@damours.org>
2014-06-30 17:27:16 -0400

    Another iteration

--- js/site.js
+++ js/site.js
@@ -1,21 +1,3 @@
-(function () {
-  var transitionTime = 0;
-  window.onscroll = function () {
-    var $body = $('body');
-    if (Date.now() - transitionTime <= 200) return;
-    var distance = $(window).scrollTop() - ($('#main').position().top / 2);
-    if ($body.hasClass('anchored') && distance < -50) {
-      $body.removeClass('anchored');
-      $('nav').fadeOut(500);
-      transitionTime = Date.now();
-    } else if (!$body.hasClass('anchored') && distance >= 0) {
-      $body.addClass('anchored');
-      $('nav').fadeIn(500);
-      transitionTime = Date.now();
-    }
-  }
-})();
-
 $(function () {
   $('div#main #what-we-do article').click(function (e) {
     e.preventDefault();
@@ -33,11 +15,46 @@ $(function () {
     }
   });
   
-  ['apprenez', 'enseignez'].forEach(function (anchor) {
-    $('body a[href="#'+ anchor +'"]').click(function (ev) {
+  var transitionTime = 0;
+  function scrolled() {
+      var $body = $('body');
+      if (Date.now() - transitionTime <= 200) return;
+      var distance = $(window).scrollTop() - ($('#main').position().top / 2);
+      if ($body.hasClass('anchored') && distance < -50) {
+        $body.removeClass('anchored');
+        $('nav').fadeOut(500);
+        transitionTime = Date.now();
+      } else if (!$body.hasClass('anchored') && distance >= 0) {
+        $body.addClass('anchored');
+        $('nav').fadeIn(500);
+        transitionTime = Date.now();
+      }
+      
+      distance = $(window).scrollTop() - ($('#main').position().top * 2 / 3);
+      if (!$('#who-we-are').hasClass('colored') && distance >= 0) {
+        $('#who-we-are').addClass('colored');
+      }
+    }
+  window.onscroll = scrolled;
+  
+  $('body')
+    .on('click', 'a[href="#join-us"], li.join-us', function (ev) {
       ev.preventDefault();
-      $('#what-we-do').attr('data-active', anchor);
-      $(window).scrollTop($('#what-we-do').offset().top - 68);
+      ev.stopImmediatePropagation();
+      $('#equipe').addClass('joining');
     })
-  })
+    .on('click', '.join-form a.close', function (ev) {
+      ev.preventDefault();
+      ev.stopImmediatePropagation();
+      $('#equipe').removeClass('joining');
+    })
+    .on('click', 'a[href^="#"]', function (ev) {
+      $.scrollTo($(this).attr('href'), 500, {
+        offset: {top: -67},
+        easing: 'easeOutQuint'
+      });
+      ev.preventDefault();
+    });
+  
+  scrolled();
 });
